98fc505a51d28c22c447e52f3d0f3b822ef61d0e
Gabriel Gatimu <geraldg652@gmail.com>
2025-02-10 10:08:00 +0300

    fix: unknown type in product bug

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,22 +1,22 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
-import {PrismaClient} from "@prisma/client";
-import {withAccelerate} from "@prisma/extension-accelerate";
-import {cloudinaryUpload} from "../utils/media.util";
-import {UploadedFile} from "express-fileupload";
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { cloudinaryUpload } from "../utils/media.util";
+import { UploadedFile } from "express-fileupload";
 
 const prisma = new PrismaClient().$extends(withAccelerate());
 const Product = prisma.product
 
 // @ desc --- Create new product
 // @ route  --POST-- [base_api]/products
-export const addProduct = asyncHandler(async (req: Request, res: Response) => {
+export const createProduct = asyncHandler(async (req: Request, res: Response) => {
     const {
         name,
         description,
         category,
         brand,
-        quantity,
+        weight,
         dimensions,
         price,
         discountRate,
@@ -30,6 +30,7 @@ export const addProduct = asyncHandler(async (req: Request, res: Response) => {
     const stockInt = parseInt(stock);
     const discountInt = parseInt(discountRate);
     const isFeatured = featured === 'true';
+    const weightInt = parseInt(weight);
 
     // @ts-ignore
     const UploadedFiles = Object.values(req.files as UploadedFile | UploadedFile[]);
@@ -46,7 +47,7 @@ export const addProduct = asyncHandler(async (req: Request, res: Response) => {
             images: imageUrls,
             category: categories,
             brand,
-            quantity,
+            weight: weightInt,
             dimensions,
             price: priceFloat,
             discountRate: discountInt,
@@ -65,13 +66,13 @@ export const addProduct = asyncHandler(async (req: Request, res: Response) => {
 // @ desc --- Update Product
 // @ route  --PUT-- [base_api]/products/:id
 export const updateProduct = asyncHandler(async (req: Request, res: Response) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const {
         name,
         description,
         category,
         brand,
-        quantity,
+        weight,
         dimensions,
         price,
         discountRate,
@@ -93,14 +94,14 @@ export const updateProduct = asyncHandler(async (req: Request, res: Response) =>
     const imageUrls = uploadImagesResult.map(image => image.secure_url)
 
     const data = await Product.update({
-        where: {id: Number(id)},
+        where: { id: Number(id) },
         data: {
             name,
             description,
             images: imageUrls,
             category: categories,
             brand,
-            quantity,
+            weight,
             dimensions,
             price: priceFloat,
             discountRate: discountInt,
@@ -121,11 +122,11 @@ export const updateProduct = asyncHandler(async (req: Request, res: Response) =>
 });
 
 // @ desc --- Get Single Product
-// @ route  --GET-- [base_api]/products/get/:id
+// @ route  --GET-- [base_api]/products/:id
 export const getProduct = asyncHandler(async (req: Request, res: Response) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const data = await Product.findUnique({
-        where: {id: Number(id)},
+        where: { id: Number(id) },
         cacheStrategy: {
             ttl: 3600, // 1 hour
             swr: 7200 // 2 hours
@@ -140,7 +141,7 @@ export const getProduct = asyncHandler(async (req: Request, res: Response) => {
     }
 
     // console.log(product)
-    res.status(200).json({data});
+    res.status(200).json({ data });
 });
 
 // @ desc --- Search for Product(s)
@@ -156,66 +157,57 @@ export const searchProduct = asyncHandler(async (req: Request, res: Response) =>
     const data = await Product.findMany({
         where: {
             OR: [
-                {name: {contains: searchTerm, mode: 'insensitive'}},
+                { name: { contains: searchTerm, mode: 'insensitive' } },
             ]
         },
         // take: 25
     })
 
-    res.status(200).json({data})
+    res.status(200).json({ data })
 })
 
 // @ desc --- Get Multiple Products
-// @ route  --GET-- [base_api]/products/all
+// @ route  --GET-- [base_api]/products
 export const fetchProducts = asyncHandler(async (req: Request, res: Response) => {
-    const {page} = req.query
-    console.log(page)
+    const { page } = req.query
 
     const pageNumber = typeof page === "string" ? Number(page) - 1 : 0
     const limit = 12 // items per page
     const skipValues = pageNumber * limit
 
-    console.log(pageNumber)
-
     const totalCount = await Product.count()
     const products = await Product.findMany({
         skip: skipValues,
         take: limit,
-        include: {
-            OrderItem: true,
-        },
-        orderBy: [{id: 'asc'}],
+        orderBy: [{ id: 'asc' }],
         cacheStrategy: {
             ttl: 60 * 5, // 5 min
             swr: 60 * 10 // 10min
         }
     });
 
-    console.log(pageNumber + 1, totalCount)
 
-        res.status(200).json({
-            totalCount,
-            page: pageNumber + 1,
-            data: products
-        });
+    res.status(200).json({
+        page: pageNumber + 1,
+        count: products.length,
+        totalCount,
+        data: products
+    });
 });
 
 // @ desc --- Get Featured Products
 // @ route  --GET-- [base_api]/products/featured
 export const fetchFeaturedProducts = asyncHandler(async (req: Request, res: Response) => {
-    const {page} = req.query
+    const { page } = req.query
     const pageNumber = typeof page === "string" ? Number(page) - 1 : 0
     const limit = 6 // items per page
     const skipValues = pageNumber * limit
 
     const data = await prisma.product.findMany({
-        where: {featured: true},
+        where: { featured: true },
         skip: skipValues,
         take: limit,
-        include: {
-            OrderItem: true,
-        },
-        orderBy: [{id: 'asc'}],
+        orderBy: [{ id: 'asc' }],
         cacheStrategy: {
             ttl: 60 * 5, // 5 min
             swr: 60 * 10 // 10min
@@ -224,6 +216,7 @@ export const fetchFeaturedProducts = asyncHandler(async (req: Request, res: Resp
 
     res.status(200).json({
         page: pageNumber + 1,
+        count: data.length,
         data
     });
 });
@@ -231,13 +224,13 @@ export const fetchFeaturedProducts = asyncHandler(async (req: Request, res: Resp
 // @ desc --- Delete Product
 // @ route  --DELETE-- [base_api]/products/:id
 export const deleteProduct = asyncHandler(async (req: Request, res: Response) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
-    const deleteProduct = await Product.delete({where: {id: Number(id)}});
+    const deleteProduct = await Product.delete({ where: { id: Number(id) } });
     if (!deleteProduct) {
         res.status(400);
         throw new Error("Failed to delete product. Try again later");
     }
 
-    res.status(200).json({message: "Product deleted successfully"});
+    res.status(200).json({ message: "Product deleted successfully" });
 })
