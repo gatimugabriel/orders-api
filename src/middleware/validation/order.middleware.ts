3f12a5624beaf4340f6ce2ee68a6003dbd7b8be5
Gabriel Gatimu <geraldg652@gmail.com>
2025-02-11 10:32:24 +0300

    add: shipping address check

--- src/middleware/validation/order.middleware.ts
+++ src/middleware/validation/order.middleware.ts
@@ -4,8 +4,9 @@ import { check } from "express-validator";
 export const validateOrderEntry = [
     check("items.*.productID", "Product ID is required").not().isEmpty(),
     check("items.*.quantity", "Quantity is required").not().isEmpty(),
-    check("items.*.quantity", "Quantity must be greater than 0").isInt({min: 1}),
-    
+    check("items.*.quantity", "Quantity must be greater than 0").isInt({min: 1}), 
+    check("shippingAddress", "Shipping Address is required").not().isEmpty(),
+
     check("deliveryMethod", "Delivery Method is required").not().isEmpty(),
     check("paymentGatewayProvider", "Payment Service Provider is required").not().isEmpty().isIn(["STRIPE", "PAYPAL", "HELCIM"]).withMessage("must be one of : STRIPE, PAYPAL, HELCIM"),
     check("paymentMethod", "Payment Service Provider is required").not().isEmpty().isIn(["CARD", "CRYPTO", "PAYPAL"]).withMessage("must be one of : CARD, PAYPAL, CRYPTO"),
