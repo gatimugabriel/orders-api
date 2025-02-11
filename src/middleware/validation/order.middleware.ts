1b853602ab8746ee66e62886b399c5f3c5f41729
Gabriel Gatimu <geraldg652@gmail.com>
2025-02-11 10:10:58 +0300

    add checks for items & payment details

--- src/middleware/validation/order.middleware.ts
+++ src/middleware/validation/order.middleware.ts
@@ -1,13 +1,13 @@
 import { check } from "express-validator";
 
-//  Order Inout Validation
+//  Order Input Validation
 export const validateOrderEntry = [
-    check("items", "Order items are required").not().isEmpty(),
-    check("totalCartValue", "Cart Value is required").not().isEmpty(),
-    check("totalCartQuantity", "Items quantity is required").not().isEmpty(),
+    check("items.*.productID", "Product ID is required").not().isEmpty(),
+    check("items.*.quantity", "Quantity is required").not().isEmpty(),
+    check("items.*.quantity", "Quantity must be greater than 0").isInt({min: 1}),
+    
     check("deliveryMethod", "Delivery Method is required").not().isEmpty(),
-    check("addressBook", "Customer phone number is required").not().isEmpty(),
-    check("isPrimaryAddress", "Address state (primary / custom) is required").not().isEmpty(),
-    check("paymentMethod", "Payment Method is required").not().isEmpty(),
-    check("paymentGatewayProvider", "Payment Service Provider is required").not().isEmpty(),
+    check("paymentGatewayProvider", "Payment Service Provider is required").not().isEmpty().isIn(["STRIPE", "PAYPAL", "HELCIM"]).withMessage("must be one of : STRIPE, PAYPAL, HELCIM"),
+    check("paymentMethod", "Payment Service Provider is required").not().isEmpty().isIn(["CARD", "CRYPTO", "PAYPAL"]).withMessage("must be one of : CARD, PAYPAL, CRYPTO"),
+    check("paymentMode", "Payment Mode is required").not().isEmpty().isIn(["ONLINE", "ONDELIVERY"]).withMessage("must be either 'ONLINE' (which means now) / 'ONDELIVERY' "),
 ]
